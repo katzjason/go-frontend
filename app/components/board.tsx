2b6d261e0661500c6ce16d343abb14511c91746f
Jason Katz <katz.jason37@gmail.com>
2025-03-17 21:50:35 -0400

    Added hover circle

--- app/components/board.tsx
+++ app/components/board.tsx
@@ -1,21 +1,58 @@
 import styles from './board.module.css';
+import React, { useState, useRef, useEffect } from 'react';
+
+interface coordinate {
+  x: number;
+  y: number;
+}
 
 export default function Board({ children }: { children: number[][] }) {
+  const [hoverCircle, setHoverCircle] = useState<coordinate | null>(null);
+  const boardRef = useRef<SVGRectElement>(null);
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (boardRef.current) {
+      const board = boardRef.current.getBoundingClientRect();
+      let mouse_x = event.clientX - board.left;
+      let mouse_y = event.clientY - board.top;
+      if (mouse_x >= 0 && mouse_x <= board.width && mouse_y >= 0 && mouse_y <= board.height) {
+        let nearest_x = Math.floor(((mouse_x / board.width) * 100) / 12.5);
+        let x_rem = ((mouse_x / board.width) * 100) % 12.5;
+        if (x_rem > 6.25) { nearest_x += 1 }
+        let nearest_y = Math.floor(((mouse_y / board.height) * 100) / 12.5);
+        let y_rem = ((mouse_y / board.height) * 100) % 12.5;
+        if (y_rem > 6.25) { nearest_y += 1 }
+        let circle_coords: coordinate = { x: nearest_x, y: nearest_y };
+        setHoverCircle(circle_coords);
+      } else {
+        setHoverCircle(null);
+      }
+    }
+  };
+
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <div>
       <svg className={styles.boardBackground}>
         <g>
-          <rect className={styles.board} width="90%" height="90%"></rect>
+          <rect ref={boardRef} className={styles.board} width="90%" height="90%"></rect>
           {/* Vertical lines */}
           {Array.from({ length: 9 }).map((_, i) => (
             <line
               key={i}
-              x1={`${5 + (i) * 11.25}%`} // Set starting X position
-              y1="5%" // Start at the top
-              x2={`${5 + (i) * 11.25}%`} // Keep same X position
-              y2="95%" // Extend to the bottom
-              stroke="black" // Line color
-              strokeWidth="2" // Line thickness
+              x1={`${5 + (i) * 11.25}%`}
+              y1="5%"
+              x2={`${5 + (i) * 11.25}%`}
+              y2="95%"
+              stroke="black"
+              strokeWidth="2"
             />
           ))}
 
@@ -23,14 +60,24 @@ export default function Board({ children }: { children: number[][] }) {
           {Array.from({ length: 9 }).map((_, i) => (
             <line
               key={i}
-              y1={`${5 + (i) * 11.25}%`} // Set starting X position
-              x1="5%" // Start at the top
-              y2={`${5 + (i) * 11.25}%`} // Keep same X position
-              x2="95%" // Extend to the bottom
-              stroke="black" // Line color
-              strokeWidth="2" // Line thickness
+              y1={`${5 + (i) * 11.25}%`}
+              x1="5%"
+              y2={`${5 + (i) * 11.25}%`}
+              x2="95%"
+              stroke="black"
+              strokeWidth="2"
             />
           ))}
+
+          {/* {Hover Circle} */}
+          {hoverCircle && (
+            <circle
+              className={styles.hoverCircle}
+              cx={`${5 + hoverCircle.x * 11.25}%`}
+              cy={`${5 + hoverCircle.y * 11.25}%`}
+              r="3.5%"
+            />
+          )}
 
           {/* Drawing Stones */}
           {children.map((row, row_i) => row.map((col, col_i) => {
@@ -55,29 +102,9 @@ export default function Board({ children }: { children: number[][] }) {
             } else {
               return
             }
-
-
-
-
           })
           )}
-
-          {/* {children.map((row, row_i) => row.map((col, col_i) => {
-            children[row_i][col_i] == 1 ?
-              <circle key={(row_i + 100 + col_i)}
-                className={styles.blackStone}
-                cx={`${5 + (col_i) * 11.25}%`}
-                cy={`${5 + (row_i) * 11.25}%`}
-                r={`${3.5}%`}></circle>
-              : (children[row_i][col_i] == -1 ?
-                <circle></circle> : null
-              )
-          }
-          ))} */}
         </g>
-        {/* <circle className={styles.whiteStone} cx="5%" cy="5%" r="3.5%"></circle>
-        <circle className={styles.blackStone} cx="5%" cy="16.25%" r="3.5%"></circle>
-        <circle className={styles.blackStone} cx="16.25%" cy="5%" r="3.5%"></circle> */}
       </svg>
     </div >
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,93 @@
-
 'use client';
 import React from 'react';
-import Link from 'next/link';
 import Layout from './components/layout';
 import Board from './components/board';
-
-
 import { useEffect, useState } from 'react';
 
 export default function Home() {
+  interface MoveData {
+    row: number;
+    col: number;
+    player_turn: number
+  }
 
-  const [board, setBoard] = useState([]);
+  interface BoardState {
+    move: MoveData,
+    board: number[][];
+    turns: number[][];
+    blackPrisoners: number;
+    whitePrisoners: number;
+    ko_x: number;
+    ko_y: number;
+    ko_player_restriction: number;
+    last_black_move: String;
+    last_white_move: String;
+    this_turn: number;
+  }
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/board")
-      .then((response) => response.json())
-      .then((data) => setBoard(data))
-      .catch((error) => console.error("Error fetchiing board", error))
-  }, []);
+  let this_turn = 1;
+  const [board, setBoard] = useState<number[][]>([]);
+
+  const Payload = async () => {
+    const handleMove = async (params: BoardState) => {
+      const { move, board, blackPrisoners, whitePrisoners, ko_x, ko_y, ko_player_restriction } = params;
+      try {
+        const response = await fetch("http://localhost:8080/api/board/move", {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(params)
+        });
+
+        if (response.ok) {
+          const responseJson = await response.json();
+          setBoard(responseJson.board);
+        } else {
+          throw response.statusText;
+        }
+      } catch (error) {
+        console.error("Failed to send payload to Java backend: ", error);
+      };
+
+    };
+
+    const move: MoveData = {
+      row: 5,
+      col: 6,
+      player_turn: -1,
+    }
+
+    let board = []
+    for (let i = 0; i < 9; i++) {
+      let row = [];
+      for (let j = 0; j < 9; j++) {
+        row.push(0);
+      }
+      board.push(row);
+    }
+    let turns = [...board]; // shallow copy of board since everything will be 0 to start
+
+    const state: BoardState = {
+      move: move,
+      board: board,
+      turns: turns,
+      blackPrisoners: 1,
+      whitePrisoners: 2,
+      ko_x: 3,
+      ko_y: 4,
+      ko_player_restriction: 1,
+      last_black_move: "",
+      last_white_move: "",
+      this_turn: this_turn
+    }
+
+    await handleMove(state);
+  };
+  // Payload();
+  console.log("FINISHED");
 
   return (
     <Layout>
+      {/* <Board>{board}</Board> */}
       <Board>{board}</Board>
     </Layout>
   );
