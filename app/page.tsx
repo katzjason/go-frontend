6d3bd3652afce0ed11622cad977304467b634e73
Jason Katz <katz.jason37@gmail.com>
2025-03-19 19:04:32 -0400

    Implemented basic move-making + capturing

--- app/page.tsx
+++ app/page.tsx
@@ -4,95 +4,93 @@ import Layout from './components/layout';
 import Board from './components/board';
 import { useEffect, useState } from 'react';
 
+
+interface MoveData {
+  row: number;
+  col: number;
+  player_turn: number
+};
+
+interface BoardState {
+  move: MoveData,
+  board: number[][];
+  turns: number[][];
+  blackPrisoners: number;
+  whitePrisoners: number;
+  ko_x: number;
+  ko_y: number;
+  ko_player_restriction: number;
+  last_black_move: String;
+  last_white_move: String;
+  this_turn: number;
+};
+
+const initialBoardState: BoardState = {
+  move: { row: 0, col: 0, player_turn: 1 },
+  board: Array(9).fill(Array(9).fill(0)),
+  turns: Array(9).fill(Array(9).fill(0)),
+  blackPrisoners: 0,
+  whitePrisoners: 0,
+  ko_x: -1,
+  ko_y: -1,
+  ko_player_restriction: 0,
+  last_black_move: "",
+  last_white_move: "",
+  this_turn: 0,
+};
+
+
 export default function Home() {
-  interface MoveData {
-    row: number;
-    col: number;
-    player_turn: number
-  }
+  const [boardState, setBoardState] = useState<BoardState>(initialBoardState);
+  const sendRequest = async (state: BoardState) => {
+    try {
 
-  interface BoardState {
-    move: MoveData,
-    board: number[][];
-    turns: number[][];
-    blackPrisoners: number;
-    whitePrisoners: number;
-    ko_x: number;
-    ko_y: number;
-    ko_player_restriction: number;
-    last_black_move: String;
-    last_white_move: String;
-    this_turn: number;
-  }
+      const response = await fetch("http://localhost:8080/api/board/move", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(state)
+      });
+      if (!response.ok) throw new Error(response.statusText);
+      const responseJson = await response.json();
 
-  let this_turn = 1;
-  const [board, setBoard] = useState<number[][]>([]);
-
-  const Payload = async () => {
-    const handleMove = async (params: BoardState) => {
-      const { move, board, blackPrisoners, whitePrisoners, ko_x, ko_y, ko_player_restriction } = params;
-      try {
-        const response = await fetch("http://localhost:8080/api/board/move", {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(params)
-        });
-
-        if (response.ok) {
-          const responseJson = await response.json();
-          setBoard(responseJson.board);
-        } else {
-          throw response.statusText;
+      setBoardState((prevBoardState) => {
+        return {
+          ...prevBoardState,
+          board: responseJson.board,
+          move: { row: 0, col: 0, player_turn: responseJson.move_player },
+          turns: Array(9).fill(Array(9).fill(0)),
+          blackPrisoners: 0,
+          whitePrisoners: 0,
+          ko_x: -1,
+          ko_y: -1,
+          ko_player_restriction: 0,
+          last_black_move: "",
+          last_white_move: "",
+          this_turn: prevBoardState.this_turn + 1,
+          move_added: responseJson.added,
         }
-      } catch (error) {
-        console.error("Failed to send payload to Java backend: ", error);
-      };
+      });
 
+    } catch (error) {
+      console.error("Failed to send payload to backend: ", error);
     };
+  }
 
-    const move: MoveData = {
-      row: 5,
-      col: 6,
-      player_turn: -1,
-    }
-
-    let board = []
-    for (let i = 0; i < 9; i++) {
-      let row = [];
-      for (let j = 0; j < 9; j++) {
-        row.push(0);
+  const boardClick = ((x: number, y: number) => {
+    setBoardState((prevBoardState) => {
+      const new_boardState: BoardState = {
+        ...prevBoardState,
+        move: { row: y, col: x, player_turn: prevBoardState.move.player_turn },
       }
-      board.push(row);
-    }
-    let turns = [...board]; // shallow copy of board since everything will be 0 to start
-
-    const state: BoardState = {
-      move: move,
-      board: board,
-      turns: turns,
-      blackPrisoners: 1,
-      whitePrisoners: 2,
-      ko_x: 3,
-      ko_y: 4,
-      ko_player_restriction: 1,
-      last_black_move: "",
-      last_white_move: "",
-      this_turn: this_turn
-    }
-
-    await handleMove(state);
-  };
-  // Payload();
-  console.log("FINISHED");
-
-  const boardClick = (x: number, y: number) => {
-    console.log("Clicked: (" + x + "," + y + ")");
-  };
+      sendRequest(new_boardState);
+      return new_boardState;
+    });
+  });
 
   return (
-    <Layout>
-      {/* <Board>{board}</Board> */}
-      <Board board={board} clickCallback={boardClick}></Board>
-    </Layout>
+    < Layout >
+      <Board board={boardState.board} clickCallback={boardClick}></Board>
+    </Layout >
   );
+
 }
