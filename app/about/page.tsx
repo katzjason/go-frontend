bc5a225877b21a1a311b79e429152e63b7904b7d
Jason Katz <katz.jason37@gmail.com>
2025-08-03 21:41:30 -0400

    Added readme download button for mobile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,16 @@ import Layout from '../components/layout';
 export default function About() {
   return (
     <Layout>
-      <div className="overflow-y-auto h-screen w-full [webkitOverflowScrolling:touch]">
+      <div className="flex md:hidden flex-col items-center justify-center h-screen text-center p-4">
+        <p className="text-[rgb(var(--text))] text-xl">Alpha Go Mini README</p>
+        <a
+          href="/alpha-go-mini-readme.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-[rgb(var(--secondary))] text-white px-4 py-4 mt-4 rounded shadow"
+        >Open PDF</a>
+      </div>
+      <div className="hidden md:block overflow-y-auto h-screen w-full [webkitOverflowScrolling:touch]">
         <iframe
           src="/alpha-go-mini-readme.pdf"
           className="w-full min-h-[100vh] border-none"
